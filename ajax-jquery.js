c014f099771ac8b57a637619dcff727008d5b46f
Astwen <astrid.wennberg@hotmail.se>
2016-05-04 10:44:04 +0200

    Update ajax-jquery.js

--- ajax-jquery.js
+++ ajax-jquery.js
@@ -1,1 +1,15 @@
-$('#my-button').click(function() {console.log("Knappen klickades på")});
+$('#my-button').click(function() { 
+  console.log("Knappen klickades på!") 
+  
+  // Gör en GET request med AJAX i jQuery
+  $.get("https://api.github.com/zen", function(data, textStatus, jqXHR) {
+    // Vilken status?
+    console.log(textStatus);
+    
+    // Visa texten från servern i stycket (<p>) med id:t show-server-text
+    $('#show-server-text').text(data);
+    
+    
+  } )
+} 
+);
